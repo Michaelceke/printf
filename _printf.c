1603f4eaf2df0f17d9d53fded2567e002ed1bd2e
Michaelceke <ekemicheal@gmail.com>
2022-07-12 09:19:53 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -156,3 +156,13 @@ int print_str(va_list arg)
 
 	return (i);
 }
+
+/**
+ *  _putchar - writes the character c to stdout
+ *  @c: the character to print
+ *  Return: 0 on success and -1 error and errno is set appropiately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
