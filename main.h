49bfdea69b762d7ce85c0451b557948d2d63b951
Michaelceke <ekemicheal@gmail.com>
2022-07-12 10:01:41 -0700

    new update

--- main.h
+++ main.h
@@ -1,39 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdarg.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
-
+#include <stdarg.h>
 /**
- * struct identifierStruct- Struct op
- * @indentifier
- * @printer: The function that prints
+ * struct specifier - struct specifier
+ * @valid: the valid character.
+ * @f: the functions associated.
  */
-typedef struct identifierStruct
+typedef struct specifier
 {
-	char *indentifier;
-	int (*printer)(va_list va);
-} identifierStruct;
-
-int _putchar(char c);
+	char *valid;
+	int (*f)(va_list);
+} spec;
 int _printf(const char *format, ...);
-int print_int(va_list arg);
-int print_unsigned(va_list arg);
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_percent(void);
-void print_binary(unsigned int n, unsigned int *printed);
-int print_unsignedToBinary(va_list arg);
-int print_hex_base(va_list arg, char _case);
-int print_STR(va_list arg);
-int print_HEX(va_list arg);
-int print_hex(va_list arg);
-int print_hex_base(va_list arg, char _case);
-int print_oct(va_list arg);
-int print_unsignedIntToHex(unsigned int num, char _case);
-
-
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int _putchar(char c);
+int print_percent(va_list args);
+int (*get_func(char x))(va_list args);
 #endif
